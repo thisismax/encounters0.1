6e224279529c06a7c8866ed55eebfc2deeb79948
Max Reichlin <max.reichlin@gmail.com>
2022-08-23 18:36:12 -0700

    first commit

---
+++ website/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = SECRET_KEY
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
