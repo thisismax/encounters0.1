f24cd3f6635bc9baa71d839151d63c2109cfd1cd
Max Reichlin <max.reichlin@gmail.com>
2022-08-25 17:45:00 -0700

    Added additional tables to database

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
 db = SQLAlchemy()
 DB_NAME = "encounters.db"
+
+HOME_FOLDER = Path('.')
+WEBSITE_FOLDER = HOME_FOLDER/"website"
 
 load_dotenv()
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -19,8 +23,17 @@ def create_app():
 
     from .views import views
     from .auth import auth
+    from .models import User, Combat, Combatant
+
+    create_db(app)
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     return app
+
+def create_db(app):
+
+    if not Path.exists(WEBSITE_FOLDER/DB_NAME):
+        db.create_all(app=app)
+        print('Created Database')
