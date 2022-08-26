bcdf5ffde0c8b9d94c76c6f421c5b60737dd5a8c
Max Reichlin <max.reichlin@gmail.com>
2022-08-25 21:34:23 -0700

    created basic authentication

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,52 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from .models import User
+from . import db
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return '<p>Login</p>'
+
+    if request.method =='POST':
+        data = request.form.to_dict()
+        
+        user = User.query.filter_by(username=data['username']).first()
+
+        if user:
+            if user.verify_password(data['password']):
+                flash("Logged In",category="success")
+            else:
+                flash("Username/password incorrect",category="error")
+        else:
+            flash("Username/password incorrect",category="error")
+
+
+
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return '<p>Logout</p>'
+    return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
-    return '<p>Sign-up</p>'
+
+    if request.method =='POST':
+        data = request.form.to_dict()
+
+        # need to check a bunch of stuff:
+        #   do passwords meet requirements?
+        #   does username already exist?
+
+        if data['password1'] == data['password2']:
+            new_user = User(username=data['username'])
+            new_user.set_password(data['password1'])
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account Created",category="success")
+
+            return redirect(url_for('views.home'))
+        else:
+            flash("Error creating account",category="error")
+
+    return render_template("sign_up.html")
--- website/models.py
+++ website/models.py
@@ -1,12 +1,19 @@
 from . import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     combats = db.relationship("Combat")
 
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    username = db.Column(db.String(150), unique=True)
+    password_hash = db.Column(db.String(150))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Combat(db.Model):
